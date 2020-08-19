351fe6bb9a5ca15e59cc9b0f1905ee5942d47aa1
Jan <janvonpichowski@gmail.com>
2020-08-19 20:38:16 +0200

    Added submodule for d2 plugin (#24)
    * Added d2 submodule
    
    * Added infrastructure for d2
    
    * Updated submodule telestion-plugin-daedalus2
    
    * Updated submodule telestion-plugin-daedalus2
    
    * Explained comment

--- telestion-api/src/main/java/org/telestion/api/message/JsonMessage.java
+++ telestion-api/src/main/java/org/telestion/api/message/JsonMessage.java
@@ -1,9 +1,9 @@
-package org.telestion.core.message;
+package org.telestion.api.message;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * The base class for all messages which are automatically encoded with the {@link JsonMessageCodec}.
+ * The base class for all messages which are automatically encoded with the JsonMessageCodec.
  * The subclasses have to be valid json classes.
  * This means that they could be encoded by {@link io.vertx.core.spi.json.JsonCodec} which is backed by
  * {@link io.vertx.core.json.jackson.JacksonCodec}.
