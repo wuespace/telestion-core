d9de9886b7249e9885759170ef16225bddcbb639
Jan von Pichowski <janvonpichowski@gmail.com>
2020-08-19 12:15:56 +0200

    Added name field to class

--- telestion-core/src/main/java/org/telestion/core/message/JsonMessage.java
+++ telestion-core/src/main/java/org/telestion/core/message/JsonMessage.java
@@ -1,4 +1,11 @@
 package org.telestion.core.message;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface JsonMessage {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    default String name() {
+        return getClass().getSimpleName();
+    }
 }
--- telestion-core/src/test/java/org/telestion/core/message/JsonMessageCodecTest.java
+++ telestion-core/src/test/java/org/telestion/core/message/JsonMessageCodecTest.java
@@ -2,6 +2,7 @@ package org.telestion.core.message;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.spi.json.JsonCodec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,9 @@ public class JsonMessageCodecTest {
 
     @Test void testEncodeDecode(){
         var expected = new Position(5.3, 4.2, 7.1);
+        System.out.println(JsonCodec.INSTANCE.toString(expected));
         var buf = Buffer.buffer();
+        System.out.println(buf.toString());
         JsonMessageCodec.Instance.encodeToWire(buf, expected);
         var actual = JsonMessageCodec.Instance.decodeFromWire(0, buf);
         Assertions.assertEquals(expected, actual);
