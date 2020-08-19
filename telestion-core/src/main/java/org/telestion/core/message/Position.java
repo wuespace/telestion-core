2e8bc3bb80b72a8c43bcfa26ce2e783bb2321648
Jan von Pichowski <janvonpichowski@gmail.com>
2020-08-19 12:41:48 +0200

    Improved javadoc

--- telestion-core/src/main/java/org/telestion/core/message/Position.java
+++ telestion-core/src/main/java/org/telestion/core/message/Position.java
@@ -3,7 +3,8 @@ package org.telestion.core.message;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * Data class which contains a position. Its json looks like this: {"x":5.3,"y":4.2,"z":7.1,"name":"Position"}
+ * Data class which contains a position. Its json looks like this:
+ * <code>{"x":5.3,"y":4.2,"z":7.1,"name":"Position"}</code>
  */
 public record Position(
         @JsonProperty double x,
