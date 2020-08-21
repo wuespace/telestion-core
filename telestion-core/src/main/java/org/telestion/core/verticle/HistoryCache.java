c93d5ea5a7485e55a75c7c735655ea7d73fcd0bc
Jan <janvonpichowski@gmail.com>
2020-08-21 15:45:03 +0200

    Update telestion-core/src/main/java/org/telestion/core/verticle/HistoryCache.java
    Co-authored-by: Pablo Klaschka <pklaschka@users.noreply.github.com>

--- telestion-core/src/main/java/org/telestion/core/verticle/HistoryCache.java
+++ telestion-core/src/main/java/org/telestion/core/verticle/HistoryCache.java
@@ -45,7 +45,7 @@ public final class HistoryCache extends AbstractVerticle {
      * @param maxSize the maximum number of elements in the response. Only the number of elements in the history will be
      *                returned if this is higher than the number of elements in the history. At most, this is the number
      *                of elements are specified in the HistoryCache configuration.
-     * @param messageName the name of the messages which should be returned (eg. Position.class.getSimpleName()).
+     * @param messageName the name of the message which should be returned (e.g., <code>Position.class.getSimpleName()</code>).
      */
     public record Request(@JsonProperty String messageName, @JsonProperty int maxSize) implements JsonMessage {
         private Request(){
