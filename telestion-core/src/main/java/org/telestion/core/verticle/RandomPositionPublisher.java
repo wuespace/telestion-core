d531096363d38c35dc9ebd1f8612e20006df7485
Jan Tischhöfer <jan.tischhoefer@gmail.com>
2020-08-27 12:31:34 +0200

    Exported RandomPositionPublisher to core.verticle as test verticle

---
+++ telestion-core/src/main/java/org/telestion/core/verticle/RandomPositionPublisher.java
@@ -0,0 +1,41 @@
+package org.telestion.core.verticle;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.telestion.core.message.Address;
+import org.telestion.core.message.Position;
+
+import java.time.Duration;
+import java.util.Random;
+
+
+/**
+ * Test class. <br>
+ * Will be removed upon first release.
+ */
+public final class RandomPositionPublisher extends AbstractVerticle {
+    private static final Logger logger = LoggerFactory.getLogger(RandomPositionPublisher.class);
+    private Random rand = new Random(555326456);
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        vertx.setPeriodic(Duration.ofSeconds(3).toMillis(), timerId -> {
+            PositionPublisher();
+        });
+        startPromise.complete();
+    }
+
+    /**
+     * Publishes random Position around Kiruna.
+     */
+    private void PositionPublisher() {
+        Position pos = new Position(
+                rand.nextDouble()+67.8915,
+                rand.nextDouble()+21.0836,
+                rand.nextDouble()*0);
+        vertx.eventBus().publish(Address.outgoing(RandomPositionPublisher.class, "MockPos"), pos.json());
+        logger.info("Sending current pos: {} on {}", pos, RandomPositionPublisher.class.getName());
+    }
+}
