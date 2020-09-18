ddcc86777c2acbd9134d3bcb4aed701092d843cc
Jan von Pichowski <janvonpichowski@gmail.com>
2020-09-18 18:05:59 +0200

    Removed deprecated JsonCodec

--- modules/telestion-core/src/main/java/org/telestion/example/PositionPublisher.java
+++ modules/telestion-core/src/main/java/org/telestion/example/PositionPublisher.java
@@ -4,7 +4,6 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import org.telestion.core.message.Address;
-import org.telestion.core.message.JsonMessageCodec;
 
 import java.time.Duration;
 
@@ -33,7 +32,6 @@ public final class PositionPublisher extends AbstractVerticle {
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         //we have to register the codec for the used message! Do this in your Launcher.
-        vertx.eventBus().registerDefaultCodec(Position.class, JsonMessageCodec.instance(Position.class));
         vertx.eventBus().consumer(Address.outgoing(PositionPublisher.class), msg -> {
             System.out.println("Received message: "+msg.body());
         });
