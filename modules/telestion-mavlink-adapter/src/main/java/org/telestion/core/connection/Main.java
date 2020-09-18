1db9a3fc96b2c66bc8175651accf8fb278ae0775
Jan von Pichowski <janvonpichowski@gmail.com>
2020-09-18 20:55:06 +0200

    Added ROS dependencies

---
+++ modules/telestion-mavlink-adapter/src/main/java/org/telestion/core/connection/Main.java
@@ -0,0 +1,11 @@
+package org.telestion.core.connection;
+
+import org.telestion.example.HelloWorld;
+import org.telestion.launcher.Launcher;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
