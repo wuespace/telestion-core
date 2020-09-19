23e5d6ad32a9d06fef0bc70a45e4c550d4a07941
Jan von Pichowski <janvonpichowski@gmail.com>
2020-09-19 13:37:17 +0200

    Added new address handling

--- modules/telestion-core/src/main/java/org/telestion/core/connection/TcpConnection.java
+++ modules/telestion-core/src/main/java/org/telestion/core/connection/TcpConnection.java
@@ -6,6 +6,7 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.net.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,18 +14,32 @@ import org.telestion.api.message.JsonMessage;
 import org.telestion.core.config.Config;
 import org.telestion.core.message.Address;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  */
 public final class TcpConnection extends AbstractVerticle {
 
+    /**
+     * @param host the host to which the connection should be established or null if this is the host of the connection
+     * @param port the port of the connection host
+     * @param broadcastAddress the address to which the incoming data should be published or null if no publishing is allowed
+     * @param targetAddresses the list of addresses to which the incoming data should be send or null if no direct targets exist
+     * @param consumingAddresses the list of addresses from which data will be consumed
+     */
     private static record Configuration(
             @JsonProperty String host,
             @JsonProperty int port,
-            @JsonProperty String outAddress,
-            @JsonProperty String inAddress){
+            @JsonProperty String broadcastAddress,
+            @JsonProperty List<String> targetAddresses,
+            @JsonProperty List<String> consumingAddresses){
         private Configuration(){
-            this(null, 7777, Address.outgoing(TcpConnection.class), Address.incoming(TcpConnection.class));
+            this(null, 7777,
+                    Address.outgoing(TcpConnection.class),
+                    null,
+                    Collections.singletonList(Address.incoming(TcpConnection.class)));
         }
     }
     
@@ -39,8 +54,9 @@ public final class TcpConnection extends AbstractVerticle {
         forcedConfig = null;
     }
 
-    public TcpConnection(String host, int port, String outAddress, String inAddress){
-        forcedConfig = new Configuration(host, port, outAddress, inAddress);
+    public TcpConnection(String host, int port,
+                         String broadcastAddress, List<String> targetAddresses, List<String> consumingAddresses){
+        forcedConfig = new Configuration(host, port, broadcastAddress, targetAddresses, consumingAddresses);
     }
 
     @Override
@@ -79,14 +95,11 @@ public final class TcpConnection extends AbstractVerticle {
         var remoteHost = socket.remoteAddress().host();
         var remotePort = socket.remoteAddress().port();
         logger.info("Connection established ({})", socket.remoteAddress());
-        socket.handler(buffer -> vertx.eventBus().publish(config.outAddress(), new TcpData(
-                remoteHost, remotePort, buffer.getBytes()).json()));
+        socket.handler(buffer -> out(new TcpData(remoteHost, remotePort, buffer.getBytes()).json()));
 
-        socket.closeHandler(handler -> {
-            logger.info("Connection closed ({})", socket.remoteAddress());
-        });
+        socket.closeHandler(handler -> logger.info("Connection closed ({})", socket.remoteAddress()));
 
-        vertx.eventBus().consumer(config.inAddress(), msg -> JsonMessage.on(TcpData.class, msg, data -> {
+        consume(msg -> JsonMessage.on(TcpData.class, msg, data -> {
             if(remotePort != data.port() || !remoteHost.equals(data.address())){
                 return;
             }
@@ -96,7 +109,25 @@ public final class TcpConnection extends AbstractVerticle {
             }
             socket.write(Buffer.buffer(data.data()));
         }));
-        vertx.eventBus().publish(config.outAddress(), new TcpConnected(remoteHost, remotePort).json());
+
+        out(new TcpConnected(remoteHost, remotePort).json());
+    }
+
+    private <T> void consume(Handler<Message<T>> handler){
+        if(config.consumingAddresses() != null){
+            config.consumingAddresses().forEach(addr -> {
+                vertx.eventBus().consumer(addr, handler);
+            });
+        }
+    }
+
+    private void out(Object data){
+        if(config.broadcastAddress() != null) {
+            vertx.eventBus().publish(config.broadcastAddress(), data);
+        }
+        if(config.targetAddresses() != null){
+            config.targetAddresses().forEach(addr -> vertx.eventBus().send(addr, data));
+        }
     }
 
     private static <T> void complete(AsyncResult<T> result, Promise<?> promise, Handler<T> handler){
