5503dba116702b8261810314a7b5e049e57c188b
Jan <janvonpichowski@gmail.com>
2020-09-19 11:53:27 +0200

    Application Configuration and Launch (#76)
    * Added submodule modules/telestion-plugin-daedalus2
    
    * Added basic configuration handling
    
    * Added default config
    
    * Added hystrix circuit breaker
    
    * Clean and comments
    
    * Removed files from hystrix-dashboard
    
    * Removed Configurator
    
    * Removed unnecessary import
    
    * Added author tag
    
    * Fix dependency
    
    * Added javadoc
    
    * Removed wrongly merged file
    
    * Added removed ProtoDatabase
    
    * Improved comments

---
+++ modules/telestion-core/src/main/java/org/telestion/example/SayHello.java
@@ -0,0 +1,71 @@
+package org.telestion.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telestion.core.config.Config;
+
+import java.time.Duration;
+import java.util.UUID;
+
+/**
+ * A class which says hello and shows the usage of configuration files.
+ *
+ * @author Jan von Pichowski
+ */
+public final class SayHello extends AbstractVerticle {
+
+    private static final Logger logger = LoggerFactory.getLogger(SayHello.class);
+
+    /**
+     * Define a configuration record
+     */
+    private static record Configuration(
+            @JsonProperty long period,
+            @JsonProperty String message) {
+
+        /**
+         * The default values will be set via the constructor
+         */
+        private Configuration(){
+            this(1, "Hello World");
+        }
+    }
+
+    /**
+     * A random uuid which defines this instance
+     */
+    private final UUID uuid = UUID.randomUUID();
+    /**
+     * The forced configuration defined by the construtor
+     */
+    private final Configuration forcedConfig;
+
+    /**
+     * No forced config is used. The config will be read from the config file or the default values will be used if the
+     * config file is not available.
+     */
+    public SayHello(){
+        this.forcedConfig = null;
+    }
+
+    /**
+     * The given forced config is used.
+     *
+     * @param period
+     * @param message
+     */
+    public SayHello(long period, String message){
+        this.forcedConfig = new Configuration(period, message);
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        var config = Config.get(forcedConfig, config(), Configuration.class);
+        vertx.setPeriodic(Duration.ofSeconds(config.period()).toMillis(), timerId -> System.out.println(config.message()+" from "+uuid));
+        startPromise.complete();
+        logger.info("Started {} with config {}", SayHello.class.getSimpleName(), config);
+    }
+}
