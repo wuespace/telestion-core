697187f2a5129b99c5b750aeea8ef1ecfda77cad
Jan von Pichowski <janvonpichowski@gmail.com>
2020-09-28 17:15:46 +0200

    Fixed path in linter_js

--- .github/workflows/parse-and-annotate-super-linter-log_js.js
+++ .github/workflows/parse-and-annotate-super-linter-log_js.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const path = require('path');
 
 const contents = fs.readFileSync(
-	path.join(__dirname, 'super-linter.log')
+	path.join(__dirname, '../../super-linter.log')
 ).toString();
 
 const lines = contents.split('\n');
