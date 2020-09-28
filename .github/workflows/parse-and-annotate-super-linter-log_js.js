af0770d53a609ccd901be3414899da2f82e0804c
Jan von Pichowski <janvonpichowski@gmail.com>
2020-09-28 17:05:10 +0200

    Fixed encoding

--- .github/workflows/parse-and-annotate-super-linter-log_js.js
+++ .github/workflows/parse-and-annotate-super-linter-log_js.js
@@ -8,27 +8,27 @@
 
 const fs = require('fs');
 const path = require('path');
-​
+
 const contents = fs.readFileSync(
-	path.join(__dirname, '../../super-linter.log')
+	path.join(__dirname, 'super-linter.log')
 ).toString();
-​
+
 const lines = contents.split('\n');
 const errors = (
 	lines.filter(
 		line=>line.startsWith('[ERROR]')
 	)
 );
-​
+
 for (let error of errors) {
 	const results = 
 		  error.match(/\[ERROR\] ([^:]+):(\d+)(:(\d+))?: (.*)/);
-​
+
 	const file = results[1];
 	const line = results[2];
 	const col = results[4];
 	const message = results[5];
-​
+
 	console.log(
 		`::error file=${file},line=${line}${col ? `,col=${col}` : ''}::${message}`
 	);
