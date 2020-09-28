cda50eb614bf5379868b9e48be486166d3d95d41
Jan von Pichowski <janvonpichowski@gmail.com>
2020-09-28 16:46:05 +0200

    Added annotations for linter

---
+++ .github/workflows/parse-and-annotate-super-linter-log.js
@@ -0,0 +1,35 @@
+/**
+ * Converts the output of the GitHub Super Linter (from a super-linter.log 
+ * file in the same directory) to GitHub annotations. Meant to be run in a
+ * GitHub Action workflow.
+ *
+ * @author Pablo Klaschka <contact@pabloklaschka.de>
+ */
+​
+const fs = require('fs');
+const path = require('path');
+​
+const contents = fs.readFileSync(
+	path.join(__dirname, '../../super-linter.log')
+).toString();
+​
+const lines = contents.split('\n');
+const errors = (
+	lines.filter(
+		line=>line.startsWith('[ERROR]')
+	)
+);
+​
+for (let error of errors) {
+	const results = 
+		  error.match(/\[ERROR\] ([^:]+):(\d+)(:(\d+))?: (.*)/);
+​
+	const file = results[1];
+	const line = results[2];
+	const col = results[4];
+	const message = results[5];
+​
+	console.log(
+		`::error file=${file},line=${line}${col ? `,col=${col}` : ''}::${message}`
+	);
+}
