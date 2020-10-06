6d7847654e5fadf23a5ed1f860c19c1a1e3e7562
Jan von Pichowski <janvonpichowski@gmail.com>
2020-10-06 12:42:24 +0200

    Fixed javdoc in record

--- modules/telestion-mavlink/src/main/java/org/telestion/protocol/mavlink/AddressPort.java
+++ modules/telestion-mavlink/src/main/java/org/telestion/protocol/mavlink/AddressPort.java
@@ -5,19 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * Linking an address and a port of a net-address.
  *
+ * @param address Actual address of a net-address.
+ * @param port Port of a net-address.
+ *
  * @author Cedric Boes
  * @version 1.0
  */
 @SuppressWarnings("preview")
-public final record AddressPort(
-        /**
-         * Actual address of a net-address.
-         */
-        @JsonProperty String address,
-        /**
-         * Port of a net-address.
-         */
-        @JsonProperty int port) {
+public final record AddressPort(@JsonProperty String address, @JsonProperty int port) {
 
     /**
      * There shall be no default constructor for normal people.<br>
