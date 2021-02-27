c6b9c9a65518148357f6e1b43b589028ffb56cb6
Jan von Pichowski <janvonpichowski@gmail.com>
2021-02-28 00:10:36 +0100

    Fixed import

--- modules/telestion-application/src/main/java/org/telestion/application/Telestion.java
+++ modules/telestion-application/src/main/java/org/telestion/application/Telestion.java
@@ -10,7 +10,7 @@ import io.vertx.core.Vertx;
 import java.util.Collections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.telestion.api.config.config.Configuration;
+import org.telestion.core.config.Configuration;
 
 /**
  * This is going to be the Telestion application. It launches the verticles which are specified in the configuration.
