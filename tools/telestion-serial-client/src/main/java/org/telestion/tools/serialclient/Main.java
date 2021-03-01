bc54c065c211e0f00fdc823bb44e371da05b0779
Jan von Pichowski <janvonpichowski@gmail.com>
2021-03-01 22:59:11 +0100

    Removed Circuit Breaker

---
+++ tools/telestion-serial-client/src/main/java/org/telestion/tools/serialclient/Main.java
@@ -0,0 +1,10 @@
+package org.telestion.tools.serialclient;
+
+import org.telestion.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.main(args);
+	}
+}
