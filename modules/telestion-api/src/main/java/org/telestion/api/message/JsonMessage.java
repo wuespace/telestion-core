8d54455797741cef66e472708de9bd89119b2515
Cedric <cedric.boes@online.de>
2021-03-17 16:02:57 +0100

    Improving Code quality of JsonMessage.java
    - Removing duplicate code parts from Json-Message
    - Update .gitignore with venv/ for python

--- modules/telestion-api/src/main/java/org/telestion/api/message/JsonMessage.java
+++ modules/telestion-api/src/main/java/org/telestion/api/message/JsonMessage.java
@@ -1,12 +1,13 @@
 package org.telestion.api.message;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonEncoding;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.jackson.JacksonCodec;
 import io.vertx.core.spi.json.JsonCodec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The base class for all messages which are automatically encoded with the JsonMessageCodec.<br>
@@ -14,11 +15,12 @@ import io.vertx.core.spi.json.JsonCodec;
  * {@link io.vertx.core.spi.json.JsonCodec} which is backed by {@link io.vertx.core.json.jackson.JacksonCodec}.
  *
  * @author Jan von Pichowski, Cedric Boes
- * @version 1.1
+ * @version 1.2
  */
 @SuppressWarnings("preview")
 public interface JsonMessage {
-	static JsonCodec JSON_CODEC = new JacksonCodec();
+	JsonCodec JSON_CODEC = new JacksonCodec();
+	Logger logger = LoggerFactory.getLogger(JsonMessage.class);
 	/**
 	 * This method decodes a {@link JsonMessage} from the event bus.<br>
 	 * Returns whether decoding was successful or not.
@@ -31,17 +33,7 @@ public interface JsonMessage {
 	 */
 	static <T extends JsonMessage> boolean on(Class<T> clazz, Object msgBody, Handler<T> handler) {
 		if (msgBody instanceof JsonObject jsonObject && jsonObject.containsKey("className")) {
-			try {
-				var msgClazz = Class.forName(jsonObject.getString("className"));
-				if (!clazz.isAssignableFrom(msgClazz)) {
-					return false;
-				}
-				handler.handle((clazz.cast(jsonObject.mapTo(msgClazz))));
-				return true;
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				return false;
-			}
+			return doReflection(clazz, jsonObject, handler);
 		} else {
 			return false;
 		}
@@ -59,17 +51,7 @@ public interface JsonMessage {
 	 */
 	static <T extends JsonMessage> boolean on(Class<T> clazz, Message<?> msg, Handler<T> handler) {
 		if (msg.body() instanceof JsonObject jsonObject && jsonObject.containsKey("className")) {
-			try {
-				var msgClazz = Class.forName(jsonObject.getString("className"));
-				if (!clazz.isAssignableFrom(msgClazz)) {
-					return false;
-				}
-				handler.handle((clazz.cast(jsonObject.mapTo(msgClazz))));
-				return true;
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				return false;
-			}
+			return doReflection(clazz, jsonObject, handler);
 		} else {
 			return false;
 		}
@@ -116,5 +98,29 @@ public interface JsonMessage {
 	 */
 	default JsonObject json() {
 		return JsonObject.mapFrom(this);
+	}
+
+	/**
+	 * Reflection part of {@link #on(Class, Message, Handler)} and {@link #on(Class, Object, Handler)}.
+	 *
+	 * @param clazz			Class to cast to
+	 * @param jsonObject	Object to be casted
+	 * @param handler		Handler for the message
+	 * @param <T>			Type of the message
+	 * @return if parsing successful
+	 */
+	private static <T extends JsonMessage> boolean doReflection(Class<T> clazz, JsonObject jsonObject,
+																Handler<T> handler) {
+		try {
+			var msgClazz = Class.forName(jsonObject.getString("className"));
+			if (!clazz.isAssignableFrom(msgClazz)) {
+				return false;
+			}
+			handler.handle((clazz.cast(jsonObject.mapTo(msgClazz))));
+			return true;
+		} catch (ClassNotFoundException e) {
+			logger.error("Error while converting Json into JsonMessage!", e);
+			return false;
+		}
 	}
 }
