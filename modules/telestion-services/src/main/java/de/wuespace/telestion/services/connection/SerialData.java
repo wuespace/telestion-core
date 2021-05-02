0613e7de4c0ae45c04cd77028c7cc902c586fde5
Cedric Bös <cedric.boes@online.de>
2021-05-02 15:47:02 +0200

    feat: Deprecate old connection-api

--- modules/telestion-services/src/main/java/de/wuespace/telestion/services/connection/SerialData.java
+++ modules/telestion-services/src/main/java/de/wuespace/telestion/services/connection/SerialData.java
@@ -3,6 +3,7 @@ package de.wuespace.telestion.services.connection;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import de.wuespace.telestion.api.message.JsonMessage;
 
+@Deprecated(since = "v0.1.3", forRemoval = true)
 public record SerialData(@JsonProperty byte[] data) implements JsonMessage {
 
 	@SuppressWarnings("unused")
