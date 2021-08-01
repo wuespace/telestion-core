ea4fa773a9b075aa74ef49b4f448991f584ac4d0
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-08-02 00:49:03 +0200

    refactor(application): Combine duplicated code in Launcher class
    Co-authored-by: Cedric Bös <16071970+cb0s@users.noreply.github.com>

--- modules/telestion-application/src/main/java/de/wuespace/telestion/launcher/Launcher.java
+++ modules/telestion-application/src/main/java/de/wuespace/telestion/launcher/Launcher.java
@@ -1,9 +1,13 @@
 package de.wuespace.telestion.launcher;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
+
 import java.time.Duration;
 import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,19 +40,13 @@ public final class Launcher {
 	public static void start(String... verticleNames) {
 		logger.info("Deploying {} verticles", verticleNames.length);
 		var vertx = Vertx.vertx();
-		// vertx.eventBus().registerDefaultCodec(Position.class, JsonMessageCodec.Instance(Position.class));
-		Arrays.stream(verticleNames).forEach(verticleName -> {
-			logger.info("Deploying verticle {}", verticleName);
-			vertx.setPeriodic(Duration.ofSeconds(5).toMillis(), timerId -> {
-				vertx.deployVerticle(verticleName, res -> {
-					if (res.failed()) {
-						logger.error("Failed to deploy verticle {} retrying in 5s", verticleName, res.cause());
-						return;
-					}
-					logger.info("Deployed verticle {} with id {}", verticleName, res.result());
-					vertx.cancelTimer(timerId);
-				});
-			});
+
+		Arrays.stream(verticleNames).forEach(name -> {
+			logger.info("Deploying verticle {}", name);
+			vertx.setPeriodic(
+					Duration.ofSeconds(5).toMillis(),
+					timerId -> vertx.deployVerticle(name, deploymentHandler(vertx, name, timerId))
+			);
 		});
 	}
 
@@ -61,19 +59,27 @@ public final class Launcher {
 	public static void start(Verticle... verticles) {
 		logger.info("Deploying {} verticles", verticles.length);
 		var vertx = Vertx.vertx();
-		// vertx.eventBus().registerDefaultCodec(Position.class, JsonMessageCodec.Instance(Position.class));
-		Arrays.stream(verticles).forEach(verticleName -> {
-			logger.info("Deploying verticle {}", verticleName);
-			vertx.setPeriodic(Duration.ofSeconds(5).toMillis(), timerId -> {
-				vertx.deployVerticle(verticleName, res -> {
-					if (res.failed()) {
-						logger.error("Failed to deploy verticle {} retrying in 5s", verticleName, res.cause());
-						return;
-					}
-					logger.info("Deployed verticle {} with id {}", verticleName, res.result());
-					vertx.cancelTimer(timerId);
-				});
-			});
+
+		Arrays.stream(verticles).forEach(instance -> {
+			logger.info("Deploying verticle {}", instance);
+			vertx.setPeriodic(
+					Duration.ofSeconds(5).toMillis(),
+					timerId -> vertx.deployVerticle(
+							instance,
+							deploymentHandler(vertx, instance.getClass().getName(), timerId)
+					)
+			);
 		});
+	}
+
+	private static Handler<AsyncResult<String>> deploymentHandler(Vertx vertx, String verticle, Long timerId) {
+		return res -> {
+			if (res.failed()) {
+				logger.error("Failed to deploy verticle {} retrying in 5s", verticle, res.cause());
+				return;
+			}
+			logger.info("Deployed verticle {} with id {}", verticle, res.result());
+			vertx.cancelTimer(timerId);
+		};
 	}
 }
