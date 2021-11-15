a2d3b08d44b60b0b5898b1b8290fb239c3487b2b
Cedric <cedric.boes@online.de>
2021-11-15 16:34:22 +0100

    feat(api): Delete redundant class parameter from `Config.get()` when using with default config

--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/config/Config.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/config/Config.java
@@ -30,11 +30,12 @@ public final class Config {
 	 * @param defaultConfig this config contains the default values which replace absent values in the JSON config
 	 * @param jsonConfig    this is common config which represents the config class in json. If fields are not available
 	 *                      they will be set like the default values of the class when the default constructor is called.
-	 * @param type          the type of the config.
 	 * @param <T>           defines the class type of the configuration
 	 * @return the selected configuration
 	 */
-	public static <T> T get(T forcedConfig, T defaultConfig, JsonObject jsonConfig, Class<T> type) {
+	@SuppressWarnings("unchecked")
+	public static <T> T get(T forcedConfig, T defaultConfig, JsonObject jsonConfig) {
+		var type = (Class<T>) defaultConfig.getClass();
 		var config = JsonObject.mapFrom(defaultConfig).mergeIn(jsonConfig);
 		return forcedConfig == null ? config.mapTo(type) : forcedConfig;
 	}
