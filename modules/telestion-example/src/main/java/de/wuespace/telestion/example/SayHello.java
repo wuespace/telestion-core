83a730e396cffb3ba10ed7640fee15808aa40f20
Pablo Klaschka <contact@pabloklaschka.de>
2021-11-20 15:01:56 +0100

    fix(api): Fix various `TelestionVerticle` issues

--- modules/telestion-example/src/main/java/de/wuespace/telestion/example/SayHello.java
+++ modules/telestion-example/src/main/java/de/wuespace/telestion/example/SayHello.java
@@ -15,27 +15,28 @@ import java.time.Duration;
  * @author Jan von Pichowski
  */
 public final class SayHello extends TelestionVerticle {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		var vertx = Vertx.vertx();
-		System.out.println("Deploying verticle");
-		vertx.deployVerticle(SayHello.class, new DeploymentOptions().setConfig(new Configuration(1, "hello world").json()));
-		System.out.println("after deploy verticle");
+		vertx.deployVerticle(SayHello.class,
+				new DeploymentOptions().setConfig(new Configuration(1, "hello world").json()));
+	}
+
+	@Override
+	public void onStart(Promise startPromise) {
+		vertx.setPeriodic(Duration.ofSeconds(getGenericConfig().getInteger("period")).toMillis(),
+				timerId -> logger.info(
+						"{} from {}",
+						getGenericConfig().getString("message"),
+						deploymentID()
+				)
+		);
+		startPromise.complete();
+		logger.info("Started {} with config {}", SayHello.class.getSimpleName(), getConfig());
 	}
 
 	public static record Configuration(
 			@JsonProperty long period,
 			@JsonProperty String message
 	) implements TelestionConfiguration {
-	}
-
-	@Override
-	public void onStart(Promise<Void> startPromise) {
-		logger.debug("onStart");
-		vertx.setPeriodic(Duration.ofSeconds(getGenericConfig().getInteger("period")).toMillis(),
-				timerId -> System.out.println(getGenericConfig().getString("message") + " from " + deploymentID()));
-		logger.debug("before promise complete");
-		startPromise.complete();
-		logger.debug("after promise complete");
-		logger.info("Started {} with config {}", SayHello.class.getSimpleName(), getConfig());
 	}
 }
