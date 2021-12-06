c311bc090d1320da03a84ec2747d04c553be2cff
Cedric Bös <cedric.boes@online.de>
2021-12-06 15:03:33 +0100

    feat: Add `Config.getUnsafe()` and restore old `Config.get()`

--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/config/Config.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/config/Config.java
@@ -6,7 +6,7 @@ import io.vertx.core.json.JsonObject;
 /**
  * A utility class for the selection of configuration files.
  *
- * @author Jan von Pichowski
+ * @author Jan von Pichowski, Cedric Boes
  */
 public final class Config {
 
@@ -27,16 +27,35 @@ public final class Config {
 	/**
 	 * Selects the right configuration file.
 	 *
-	 * @param forcedConfig  this config will be applied if it is not null
+	 * @param forcedConfig	this config will be applied if it is not null
+	 * @param defaultConfig this config contains the default values which replace absent values in the JSON config
+	 * @param jsonConfig	this is common config which represents the config class in json. If fields are not available
+	 *                     	they will be set like the default values of the class when the default constructor is
+	 *                     	called
+	 * @param type       	the type of the config.
+	 * @param <T>          	defines the class type of the configuration
+	 * @return the selected configuration
+	 */
+	public static <T> T get(T forcedConfig, T defaultConfig, JsonObject jsonConfig, Class<T> type) {
+		var config = JsonObject.mapFrom(defaultConfig).mergeIn(jsonConfig);
+		return forcedConfig == null ? config.mapTo(type) : forcedConfig;
+	}
+
+	/**
+	 * Selects the right configuration file without the class object like
+	 * {@link #get(Object, Object, JsonObject, Class)}.
+	 *
+	 * @param forcedConfig	this config will be applied if it is not null
 	 * @param defaultConfig this config contains the default values which replace absent values in the JSON config;
 	 *                      it must not be <code>null</code>!
 	 * @param jsonConfig    this is common config which represents the config class in json. If fields are not available
-	 *                      they will be set like the default values of the class when the default constructor is called.
+	 *                      they will be set like the default values of the class when the default constructor is
+	 *                      called.
 	 * @param <T>           defines the class type of the configuration
 	 * @return the selected configuration
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T get(T forcedConfig, @NonNull T defaultConfig, JsonObject jsonConfig) {
+	public static <T> T getUnsafe(T forcedConfig, @NonNull T defaultConfig, JsonObject jsonConfig) {
 		var type = (Class<T>) defaultConfig.getClass();
 		var config = JsonObject.mapFrom(defaultConfig).mergeIn(jsonConfig);
 		return forcedConfig == null ? config.mapTo(type) : forcedConfig;
