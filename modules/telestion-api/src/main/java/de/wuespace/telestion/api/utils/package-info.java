7479a1881f9d528e8d3dca1ecad21d02b56a7ce7
Ludwig Richter <ludwig.richter@wuespace.de>
2021-12-20 01:21:12 +0100

    docs(api): Add first package information
    Co-authored-by: Cedric Bös <16071970+cb0s@users.noreply.github.com>

---
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/utils/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <h2>Telestion API utilities</h2>
+ *
+ * Contains general utility classes for the Telestion ecosystem.
+ * <p>
+ * To indicate a class is a Telestion-utility class,
+ * it extends {@link de.wuespace.telestion.api.utils.AbstractUtils AbstractUtils}.
+ * When trying to instantiate them, an {@link java.lang.UnsupportedOperationException} is thrown to inform the user of
+ * the api of the incorrect usage.
+ * <p>
+ * (c) WueSpace e.V.
+ */
+package de.wuespace.telestion.api.utils;
