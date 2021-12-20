03526b1f07653d1933ec54aca72b72cdc85a619a
Cedric <cedric.boes@online.de>
2021-12-20 16:19:23 +0100

    docs(api): Add missing documentation for `CommonNames` and `InformationCodec`

--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/message/header/InformationCodec.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/message/header/InformationCodec.java
@@ -7,18 +7,45 @@ import de.wuespace.telestion.api.utils.PrimitiveTypeParser;
 import java.util.Objects;
 
 /**
+ * A utility class helping to encode and decode {@link Information} with the {@link PrimitiveTypeParser}.
+ *
  * @author Ludwig Richter (@fussel178), Cedric Boes (@cb0s)
  */
 public class InformationCodec extends AbstractUtils {
+	/**
+	 * Encodes a given object by calling {@link String#valueOf(Object)}. If the specified object is primitive,
+	 * the corresponding variant of {@code String.valueOf()} is called.
+	 *
+	 * @param value to get encode as a {@link String}
+	 * @return String representation of the given object
+	 */
 	public static String encode(Object value) {
 		if (Objects.isNull(value)) return null;
 		return String.valueOf(value);
 	}
 
+	/**
+	 * Decodes a given encoded {@link String} and tries to map it to a primitive datatype (or String). It is a wrapper
+	 * to the {@link PrimitiveTypeParser}.
+	 *
+	 * @param encoded			{@link String} to decode
+	 * @param primitiveClass	type to map to
+	 * @param <T>	type to decode to
+	 * @return	decoded primitive type from the given {@link String}
+	 * @throws NoSuchPrimitiveTypeException if decoding fails
+	 */
 	public static <T> T decode(String encoded, Class<T> primitiveClass) throws NoSuchPrimitiveTypeException {
 		return PrimitiveTypeParser.parse(encoded, primitiveClass);
 	}
 
+	/**
+	 * Like {@link #decode(String, Class)} but decoded result is unsigned.
+	 *
+	 * @param encoded        the encoded/raw value as string
+	 * @param primitiveClass the class of the primitive type to try to parse to
+	 * @param <T>            class representation of the primitive type
+	 * @throws NoSuchPrimitiveTypeException if decoding fails
+	 */
 	public static <T> T decodeUnsigned(String encoded, Class<T> primitiveClass) throws NoSuchPrimitiveTypeException {
 		return PrimitiveTypeParser.parseUnsigned(encoded, primitiveClass);
 	}
--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/message/header/serialization/CommonNames.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/message/header/serialization/CommonNames.java
@@ -14,20 +14,61 @@ import de.wuespace.telestion.api.utils.AbstractUtils;
  */
 public class CommonNames extends AbstractUtils {
 	// serial stuff
+	/**
+	 * Recommended name to specify the serial device.
+	 */
 	public static final String SERIAL_DEVICE = "serial-device";
+	/**
+	 * Recommended name to specify the serial baudrate.
+	 */
 	public static final String SERIAL_BAUDRATE = "serial-baudrate";
 	// ip stuff
+	/**
+	 * Recommended name to specify the ip address in the ip protocol.
+	 */
 	public static final String HOST_IP = "host-ip";
+	/**
+	 * Recommended name to specify the host name in the ip protocol.
+	 */
 	public static final String HOST_NAME = "host-name";
+	/**
+	 * Recommended name to specify the port of the host in the ip protocol.
+	 */
 	public static final String HOST_PORT = "host-port";
 	// message stuff
+	/**
+	 * Recommended name for some type of message id in the header.
+	 */
 	public static final String MESSAGE_ID = "message-id";
+	/**
+	 * Recommended name for some type of message name in the header.
+	 */
 	public static final String MESSAGE_NAME = "message-name";
+	/**
+	 * Recommended name for the type of message.
+	 */
 	public static final String MESSAGE_TYPE = "message-type";
 	// time stuff
+	/**
+	 * Recommended name for when the package got created.
+	 */
 	public static final String TIME_CREATED = "time-created";
+	/**
+	 * Recommended name for when a package was last modified.
+	 */
 	public static final String TIME_MODIFIED = "time-modified";
+	/**
+	 * Recommended name for when a package was last accessed.
+	 */
 	public static final String TIME_ACCESSED = "time-accessed";
+	/**
+	 * Recommended name for when a package was received.
+	 */
 	public static final String TIME_RECEIVED = "time-received";
+	/**
+	 * Recommended name for communicating the current time to another verticle.
+	 */
 	public static final String TIME_CURRENT = "time-current";
+
+	// This list is incomplete and will be extended in the future
 }
