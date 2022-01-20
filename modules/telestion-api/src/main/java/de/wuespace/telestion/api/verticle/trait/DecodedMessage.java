ddb8bda9041950e675e871ad789ed25ab497045c
Ludwig Richter <ludwig.richter@wuespace.de>
2022-01-20 04:40:02 +0100

    feat(api): Update request methods in `WithEventBus` trait

--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/verticle/trait/DecodedMessage.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/verticle/trait/DecodedMessage.java
@@ -3,9 +3,10 @@ package de.wuespace.telestion.api.verticle.trait;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import de.wuespace.telestion.api.message.JsonMessage;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
 
-public record DecodedMessage<V extends JsonMessage, T>(
+public record DecodedMessage<V extends JsonMessage, T extends JsonObject>(
 		@JsonProperty V body,
 		@JsonProperty Message<T> message
-		) implements JsonMessage {
+) implements JsonMessage {
 }
