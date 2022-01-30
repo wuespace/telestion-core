3a661f51523a91529eceed6a5fa826b8eee51a1b
Ludwig Richter <ludwig.richter@wuespace.de>
2022-01-31 00:54:56 +0100

    docs(api): Add missing class and method documentation

--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/verticle/GenericConfiguration.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/verticle/GenericConfiguration.java
@@ -1,4 +1,11 @@
 package de.wuespace.telestion.api.verticle;
 
+/**
+ * The type of configuration for a {@link TelestionVerticle} that you use
+ * to indicate that the verticle does not have a strictly typed configuration.
+ *
+ * @author Pablo Klaschka (@pklaschka), Ludwig Richter (@fussel178)
+ * @see TelestionVerticle#getConfig()
+ */
 public record GenericConfiguration() implements TelestionConfiguration {
 }
--- modules/telestion-api/src/main/java/de/wuespace/telestion/api/verticle/trait/MessageHandler.java
+++ modules/telestion-api/src/main/java/de/wuespace/telestion/api/verticle/trait/MessageHandler.java
@@ -2,6 +2,13 @@ package de.wuespace.telestion.api.verticle.trait;
 
 import de.wuespace.telestion.api.message.JsonMessage;
 
+/**
+ * An event handler which accepts the decoded body of the message
+ * and returns nothing.
+ *
+ * @author Ludwig Richter (@fussel178)
+ * @see WithEventBus#register(String, MessageHandler, Class)
+ */
 @FunctionalInterface
 public interface MessageHandler<T extends JsonMessage> {
 	void handle(T body);
