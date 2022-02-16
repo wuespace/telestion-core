12a26b349aaf9af6b041f4552c2786c0d5be9849
Ludwig Richter <ludwig.richter@wuespace.de>
2022-02-16 17:08:59 +0100

    feat(application): Extend the `DeploymentLauncher` interface by default methods to easier deploy verticles on the controlled Vert.x instance

--- modules/telestion-application/src/main/java/de/wuespace/telestion/application/launcher/DeploymentLauncher.java
+++ modules/telestion-application/src/main/java/de/wuespace/telestion/application/launcher/DeploymentLauncher.java
@@ -34,4 +34,88 @@ public interface DeploymentLauncher extends LoaderLauncher {
 	 * @return the current deployments
 	 */
 	List<Deployment> getDeployments();
+
+
+	/**
+	 * Like {@link #deploy(Class)} but with custom {@link DeploymentOptions deployment options}.
+	 * These {@link DeploymentOptions deployment options} are passed to the {@link io.vertx.core.Vertx Vert.x instance}
+	 * on deployment.
+	 *
+	 * @param verticleClass the class type of the {@link Verticle verticle} that you want deploy
+	 * @param options       custom {@link DeploymentOptions deployment options} for your running verticle
+	 * @return a future which resolves with a {@link Deployment} which represents your {@link Verticle verticle}
+	 * when it is successfully deployed
+	 * @see io.vertx.core.Vertx#deployVerticle(Class, DeploymentOptions)
+	 */
+	default Future<Deployment> deploy(Class<? extends Verticle> verticleClass, DeploymentOptions options) {
+		return addDeployment(new Deployment(this, verticleClass, options)).deploy();
+	}
+
+	/**
+	 * Like {@link #deploy(Verticle)} but the {@link Verticle verticle} instance is created
+	 * by invoking the default constructor.
+	 *
+	 * @param verticleClass the class type of the {@link Verticle verticle} that you want deploy
+	 * @return a future which resolves with a {@link Deployment} which represents your {@link Verticle verticle}
+	 * when it is successfully deployed
+	 * @see io.vertx.core.Vertx#deployVerticle(Class, DeploymentOptions)
+	 */
+	default Future<Deployment> deploy(Class<? extends Verticle> verticleClass) {
+		return addDeployment(new Deployment(this, verticleClass)).deploy();
+	}
+
+	/**
+	 * Like {@link #deploy(Verticle)} but with custom {@link DeploymentOptions}.
+	 * The deployment options configure the {@link Verticle} during deployment.
+	 *
+	 * @param verticle your created verticle that you want to deploy
+	 * @param options  deployment options for your running verticle
+	 * @return a future which resolves with a {@link Deployment} which represents your verticle
+	 * when it is successfully deployed
+	 * @see io.vertx.core.Vertx#deployVerticle(Verticle, DeploymentOptions)
+	 */
+	default Future<Deployment> deploy(Verticle verticle, DeploymentOptions options) {
+		return addDeployment(new Deployment(this, verticle, options)).deploy();
+	}
+
+	/**
+	 * Deploys a {@link Verticle} on the {@link io.vertx.core.Vertx Vertx} instance
+	 * of the {@link VertxLauncher} that you have created by yourself.
+	 *
+	 * @param verticle your created verticle that you want to deploy
+	 * @return a future which resolves with a {@link Deployment} which represents your verticle
+	 * when it successfully deploys
+	 * @see io.vertx.core.Vertx#deployVerticle(Verticle)
+	 */
+	default Future<Deployment> deploy(Verticle verticle) {
+		return addDeployment(new Deployment(this, verticle)).deploy();
+	}
+
+	/**
+	 * Like {@link #deploy(String)} but with custom {@link DeploymentOptions deployment options}.
+	 * These {@link DeploymentOptions deployment options} are passed to the {@link io.vertx.core.Vertx Vert.x instance}
+	 * on deployment.
+	 *
+	 * @param className the classname or binary name of your {@link Verticle verticle} class
+	 * @param options   custom {@link DeploymentOptions deployment options} for your running verticle
+	 * @return a future which resolves with a {@link Deployment} which represents your {@link Verticle verticle}
+	 * when it is successfully deployed
+	 * @see io.vertx.core.Vertx#deployVerticle(String, DeploymentOptions)
+	 */
+	default Future<Deployment> deploy(String className, DeploymentOptions options) {
+		return addDeployment(new Deployment(this, className, options)).deploy();
+	}
+
+	/**
+	 * Like {@link #deploy(Verticle)} but the {@link Verticle verticle} instance is created
+	 * by a {@link io.vertx.core.spi.VerticleFactory verticle factory} based on the classname.
+	 *
+	 * @param className the classname or binary name of your {@link Verticle verticle} class
+	 * @return a future which resolves with a {@link Deployment} which represents your {@link Verticle verticle}
+	 * when it is successfully deployed
+	 * @see io.vertx.core.Vertx#deployVerticle(String)
+	 */
+	default Future<Deployment> deploy(String className) {
+		return addDeployment(new Deployment(this, className)).deploy();
+	}
 }
