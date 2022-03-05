43614fd4ef9582bea749a967be188707478d8936
Ludwig Richter <ludwig.richter@wuespace.de>
2022-03-05 13:23:05 +0100

    chore(application): Add an example to the `EventSelector` handle method

--- modules/telestion-application/src/main/java/de/wuespace/telestion/application/loader/EventSelector.java
+++ modules/telestion-application/src/main/java/de/wuespace/telestion/application/loader/EventSelector.java
@@ -14,6 +14,22 @@ import java.util.List;
 public interface EventSelector {
 	/**
 	 * Selects and executes an event handler on a {@link Loader}.
+	 * <p>
+	 * For example:
+	 * <pre>
+	 * {@code
+	 * // here is your loader list
+	 * List<Loader> loaders;
+	 *
+	 * // the Event Selector is basically a lambda function which selects the event
+	 * // on a loader that you want to call
+	 * Loader.call(loaders, (loader, promise) -> loader.onBeforeVertxStartup(promise));
+	 *
+	 * // due to the nature of both definitions, you can use the double-colon operator (::)
+	 * // or method reference
+	 * Loader.call(loaders, Loader::onBeforeVertxStartup);
+	 * }
+	 * </pre>
 	 *
 	 * @param loader  the loader which provides the selected event handler
 	 * @param promise a promise which receives the selected event handler
