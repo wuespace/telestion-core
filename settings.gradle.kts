7d938e6557d5130ffd600aea92d12c2cee217da8
Ludwig Richter <ludwig.richter@wuespace.de>
2022-12-21 01:23:20 +0100

    chore(gradle): Update project configuration to use the new project plugins

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "telestion-core"
+
+// by default gradle loads dependency catalogs from gradle/${name}.versions.toml
+// where ${name} is the name where the declared dependencies and bundles are available.
+// You don't need to explicitly define the "dependencyResolutionManagement" closure.
+
+// projects
+include("telestion-api")
+include("telestion-application")
+include("telestion-services")
+include("telestion-examples")
