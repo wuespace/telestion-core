34d2b5d5b16686c15f280bf275a0438c0bdfb61c
Ludwig Richter <ludwig.richter@wuespace.de>
2023-01-04 23:05:14 +0100

    chore(buildSrc): Set kotlin jvm toolchain version explicitly

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     `kotlin-dsl`
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 repositories {
     // so that external plugins can be resolved in dependencies section
     gradlePluginPortal()
